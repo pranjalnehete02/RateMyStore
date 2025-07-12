37830e4989f0ea7d6f8269e4975267afc28ed22c
Pranjal Gopal Nehete <85827353+pranjalnehete02@users.noreply.github.com>
2025-07-12 12:59:33 +0530

    loginPage CSS inline

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,39 +4,16 @@ import LoginPage from "./pages/LoginPage";
 import AdminDashboard from "./pages/AdminDashboard";
 import UserDashboard from "./pages/UserDashboard";
 import OwnerDashboard from "./pages/OwnerDashboard";
-import ProtectedRoute from "./routes/ProtectedRoute";
 
-function App() {
+const App = () => {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route
-        path="/admin"
-        element={
-          <ProtectedRoute allowedRoles={["admin"]}>
-            <AdminDashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/user"
-        element={
-          <ProtectedRoute allowedRoles={["user"]}>
-            <UserDashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/owner"
-        element={
-          <ProtectedRoute allowedRoles={["owner"]}>
-            <OwnerDashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="*" element={<p>404 Not Found</p>} />
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/user" element={<UserDashboard />} />
+      <Route path="/owner" element={<OwnerDashboard />} />
     </Routes>
   );
-}
+};
 
 export default App;
